852361950e247bd20949bd96f71b1439469c0574
Rao Fahad <raofahad046@gmail.com>
2024-05-13 20:20:25 +0500

    did some updates

--- frontend/components/common/navbar/Navbar.tsx
+++ frontend/components/common/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 // import services
-import { useContext } from 'react';
+import { useContext } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -15,63 +15,30 @@ import ROUTES from "@/utils/ROUTES";
 import AuthLinks from "./AuthLinks";
 
 // import context
-import authContext from '@/context/auth/authContext';
+import authContext from "@/context/auth/authContext";
 
 // custom interface
 interface IProps {
-  show_auth_links: boolean,
+  show_auth_links: boolean;
 }
 
 // COMPONENT
-const Navbar: React.FC<IProps> = ({
-  show_auth_links,
-}) => {
+const Navbar: React.FC<IProps> = ({ show_auth_links }) => {
   // context
   const { user } = useContext(authContext);
 
   return (
-    <nav className="
+    <nav
+      className="
       w-full h-[64px]
       flex items-center justify-between gap-4
       py-[10px] px-[20px]
       border border-b-[#dadada]
-    ">
-
-      {/* NAV-ICON AND LOGO CONTAINER */}
-      <div className="w-fit h-full flex items-center justify-center gap-1">
-        {/* Hamburger */}
-        <HiMiniBars3 className={`w-[25px] h-auto cursor-pointer active:scale-95 mr-[10px]`} />
-
-        {/* Logo */}
-        <Link className="w-fit h-full flex items-center justify-center" href={ROUTES.ROOT}>
-          <Image
-            src="/images/livecart-logo.svg"
-            width={170}
-            height={49}
-            alt="Livecart icon logo"
-            className="w-auto h-[80%] cursor-pointer"
-          />
-        </Link>
-      </div>
-
-      {
-        user ? (
-          // USER ICON
-          <Image
-            src="/images/account-logo.svg"
-            width={50}
-            height={49}
-            alt="Livecart icon logo"
-            className="w-auto h-[70%] cursor-pointer active:scale-95"
-          />
-        ) : (
-          // AUTH LINKS
-          show_auth_links && <AuthLinks />
-        )
-      }
-      
+    "
+    >
+      {/* <h3>Welcome to {user?.email}</h3> */}
     </nav>
   );
-}
+};
 
 export default Navbar;
--- frontend/types.ts
+++ frontend/types.ts
@@ -34,7 +34,9 @@ export type EmailVerificationFormData = {
     verification_code: string | null;
   };
 };
-export type EmailVerificationPurpose = 'SIGNUP_VERIFICATION' | 'EMAIL_VERIFICATION';
+export type EmailVerificationPurpose =
+  | "SIGNUP_VERIFICATION"
+  | "EMAIL_VERIFICATION";
 
 // Send Code
 export type SendCodeFormData = {
@@ -43,25 +45,24 @@ export type SendCodeFormData = {
     email: string | null;
   };
 };
-export type SendCodePurpose = 'EMAIL_VERIFICATION' | 'RESET_PASSWORD';
+export type SendCodePurpose = "EMAIL_VERIFICATION" | "RESET_PASSWORD";
 
 // Reset Password
 export type ResetPasswordFormData = {
   verification_code: string;
-  password: string,
+  password: string;
   error: {
     verification_code: string | null;
     password: string | null;
   };
 };
 
-
 // ========================================================================
 // UTILS
 // ========================================================================
 export interface ReturnTypeValidationFunction {
   // If error is null it means it is valid, otherwise it is invalid and we will also get error
-  error: string | null,
+  error: string | null;
 }
 
 // ========================================================================
@@ -69,45 +70,46 @@ export interface ReturnTypeValidationFunction {
 // ========================================================================
 
 export interface ActionType {
-  type: string,
-  payload?: any,
+  type: string;
+  payload?: any;
 }
 
 // Auth
 export interface User {
-  email: string,
+  email: string;
+  access_token: string;
 }
 export interface UpdateUserInput {
-  username?: string,
-  email?: string,
+  username?: string;
+  email?: string;
 }
 export interface AuthInitialState {
-  user: User | null,
-  loading: boolean,
-  set_auth_loading?: (value: boolean) => void,
-  update_user?: (user: UpdateUserInput) => void,
+  user: User | null;
+  loading: boolean;
+  set_auth_loading?: (value: boolean) => void;
+  update_user?: (user: UpdateUserInput) => void;
 }
 
 // Alerts
 export enum AlertType {
-  SUCCESS = 'success',
-  ERROR = 'error',
-  INFO = 'info',
-  WARNING = 'warning',
-  LOADING = 'loading'
+  SUCCESS = "success",
+  ERROR = "error",
+  INFO = "info",
+  WARNING = "warning",
+  LOADING = "loading",
 }
 export interface Alert {
-  id: string,
-  type: AlertType,
-  message: string
+  id: string;
+  type: AlertType;
+  message: string;
 }
 export interface AlertInput {
-  type: AlertType,
-  message: string
+  type: AlertType;
+  message: string;
 }
 export interface AlertInitialState {
-  alerts: Alert[],
-  add_alert?: (alert: AlertInput) => void,
-  clear_alert?: (id: string) => void,
-  clear_alert_state?: () => void,
+  alerts: Alert[];
+  add_alert?: (alert: AlertInput) => void;
+  clear_alert?: (id: string) => void;
+  clear_alert_state?: () => void;
 }
