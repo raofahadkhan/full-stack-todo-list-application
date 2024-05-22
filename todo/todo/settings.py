71c451acaccda5f4bb1e7922d747280b433d8070
RAO FAHAD <raofahad046@gmail.com>
2024-05-23 01:10:51 +0500

    did some changes

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -7,7 +7,3 @@ except FileNotFoundError:
     config = Config()
     
 DATABASE_URL = config("DATABASE_URL",cast=Secret)
-REFRESH_TOKEN_EXPIRE_DAYS = config("REFRESH_TOKEN_EXPIRE_DAYS",cast=int)
-ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES",cast=int)
-JWT_ALGORITHM = config("JWT_ALGORITHM",cast=str)
-JWT_SECRET = config("JWT_SECRET",cast=str)
